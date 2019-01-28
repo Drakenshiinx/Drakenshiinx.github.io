6c788730a6abac8ed39ab01bab4fe5ee7c5a0b75
Tiffany Cole <Drakenshiinx@hotmail.com>
2019-01-27 18:55:52 -0800

    exercise 5

---
+++ js/exercise5Color.js
@@ -0,0 +1,47 @@
+var input1 = document.querySelector("#inputRed");
+var input2 = document.querySelector("#inputGreen");
+var input3 = document.querySelector("#inputBlue");
+var button = document.querySelector("#clicker");
+var clearButton = document.querySelector("#clear");
+
+button.addEventListener("click", clickHandler, false);
+clearButton.addEventListener("click", clearColorHandler, false);
+
+function clickHandler(e) {
+	var x = parseInt( input1.value );
+	//the value of red
+    var y = parseInt( input2.value );
+	//the value of green
+    var z = parseInt( input3.value );
+	//the value of blue
+	
+    if (x < 0 || x > 255 || isNaN(x))
+    {
+    	alert("You must enter a number from 1 to 255 in Red!");
+		//does a check for values not 0-255 and if its not a number for red
+    }
+		else if (y < 0 || y > 255 || isNaN(y))
+    {
+    	alert("You must enter a number from 1 to 255 in Green!");
+		//does a check for values not 0-255 and if its not a number for green
+    }
+        else if (z < 0 || z > 255 || isNaN(z))
+    {
+    	alert("You must enter a number from 1 to 255 in Blue!");
+		//does a check for values not 0-255 and if its not a number for blue
+    }
+		else
+    {
+    
+		return colorBar.style.backgroundColor = "rgb("+x+"," +y+ "," +z+")";
+		//this fills the color bar with the RGB values
+	}
+}
+function clearColorHandler(e) {
+	clear();
+}
+
+function clear() {
+	colorBar.style.backgroundColor = "OldLace";
+	//clears the color bar
+}
