c2621b4b929de14cd8311b3ed13ab6f723db2ec3
Tiffany Cole <Drakenshiinx@hotmail.com>
2019-02-02 16:28:37 -0800

    exercise 5

---
+++ js/exercisesfive.js
@@ -0,0 +1,20 @@
+var x = 0;
+var gameList = Array();
+
+function addGame()
+{
+gameList[x] = document.getElementById("nameOfGame").value;
+x++;
+document.getElementById("nameOfGame").value;
+}
+
+function displayGames()
+{
+let e ="<hr/>";
+
+for (var z = 0; z <array.length; z++)
+{
+e += "Element " +z+ "= " + gamelist[z] + "<br/>";
+}
+document.getElementById("displayedinfo").innerHTML = e;
+}
