4e4f834102802acee20f6f89499e6f4288399fc0
Tiffany Cole <Drakenshiinx@hotmail.com>
2019-02-06 14:37:14 -0800

    starting the adventure game

---
+++ js/AdventureGame.js
@@ -0,0 +1,12 @@
+//create the array
+var map = [];
+
+map[0] = "The back patio.";
+map[1] = "The backyard.";
+map[2] = "The tool shed.";
+
+//set player location
+var mapLocation = 4;
+
+//the output element
+var output = document.querySelector("#output");
