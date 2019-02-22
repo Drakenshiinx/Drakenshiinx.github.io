9ab0a6ceb982588f6cc56192650d67835623a6ef
Tiffany Cole <Drakenshiinx@hotmail.com>
2019-02-21 22:36:25 -0800

    Update AdventureGame.js

--- js/AdventureGame.js
+++ js/AdventureGame.js
@@ -163,14 +163,14 @@ function playGame()
 		if(mapLocation % 3 != 2)
 		{
 			if(mapLocation === 2)
-		{
+			{
 				if(!backpack.includes("key"))
 				{
 					console.log("loop to check key");
 					console.log(backpack);
 					gameMessage="The door unlocks";
 				}			
-		}
+			}
 		else{
 			mapLocation += 1;
 			localStorage.setItem("playerPosition",mapLocation);
