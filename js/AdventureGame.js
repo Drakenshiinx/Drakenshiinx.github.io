17bc44571b0115acba0a82bcccbb3886248507f2
Tiffany Cole <Drakenshiinx@hotmail.com>
2019-02-25 16:49:03 -0800

    organizing/cleaning code

--- js/AdventureGame.js
+++ js/AdventureGame.js
@@ -1,4 +1,5 @@
 "use strict";
+
 var ENTER = 13;
 
 //Array to store locations on the map
@@ -42,7 +43,7 @@ blockedPath[2] = "<i>This is a dead end.  You must turn back.</i>";
 blockedPath[3] = "<i>You are a living being and are unable to phase through walls.</i>";
 blockedPath[4] = "<i>You cannot continue further.</i>";
 blockedPath[5] = "<i>lol nope.</i>";
-blockedPath[6] = "<i>The deer head mounted on the wall stares into your soul preventing you from going in that direction.</i>";
+blockedPath[6] = "<i>The deer head mounted on the wall stares into your soul preventing" + " you from going in that direction.</i>";
 
 //items array
 var gameItems = ["key", "cheese"];
@@ -79,8 +80,7 @@ var output = document.querySelector("#output");
 var input = document.querySelector("#input");
 
 //function that will bring the player to the last position they left off at
-function lastPlace() 
-{
+function lastPlace() {
   let x = localStorage.getItem("playerPosition");
   console.log("saved map location" + x);
   output.innerHTML = map[x];
@@ -99,322 +99,269 @@ window.addEventListener("keydown", keydownHandler, false);
 render();
 
 //allows the user to hit enter
-function keydownHandler(event)
-{
+function keydownHandler(event) {
   let x = localStorage.getItem("playerPosition");
-  if(event.keyCode === ENTER)
-  {
+  if (event.keyCode === ENTER) {
     playGame();
   }
 }
 
-function clickHandler()
-{
-	let x = localStorage.getItem("playerPosition");
-	playGame();
+function clickHandler() {
+  let x = localStorage.getItem("playerPosition");
+  playGame();
 }
 
-function playGame()
-{
-	//get the players input and convert it to lowercase
-	userInput = input.value;
-	userInput = userInput.toLowerCase();
-	
-	//reset these variables from the previous turn
-	gameMessage = "";
-	action = "";
-	
-	//figure out the players action
-	for(var i = 0; i < actionsIKnow.length; i++)
-	{
-		if(userInput.indexOf(actionsIKnow[i]) !== -1)
-		{
-			action = actionsIKnow[i];
-			console.log("player's action: " + action);
-			break;
-		}
-	}
-	
-	//figure out the item the player wants
-	for (i = 0; i < itemsIKnow.length; i++)
-	{
-		if(userInput.indexOf(itemsIKnow[i]) !== -1)
-		{
-			item = itemsIKnow[i];
-			console.log("Player's item: " + item);
-		}	
-	}
-	
-	//choose the correct action
-	switch(action)
-	{
-		//move north command
-		case "north":
-		if(mapLocation >= 3)
-		{
-			mapLocation -= 3;
-			localStorage.setItem("playerPosition",mapLocation);
-			console.log(mapLocation);
-		}
-		else
-		{
-			gameMessage = blockedPath[mapLocation];
-		}
-		break;
-		
-		//move east command
-		case "east":
-		if(mapLocation % 3 != 2)
-		{
-			mapLocation += 1;
-			localStorage.setItem("playerPosition",mapLocation);
-			console.log(mapLocation);
-		}
-		else
-		{
-			gameMessage = blockedPath[mapLocation];
-		}
-		break;
-		
-		//move south command
-		case "south":
-		if (mapLocation < 6)
-		{
-			mapLocation += 3;
-			localStorage.setItem("playerPosition",mapLocation);
-			console.log(mapLocation);
-		}
-		else
-		{
-			gameMessage = blockedPath[mapLocation];
-		}
-		break;
-		
-		//move west command
-		case "west":
-		if(mapLocation % 3 != 0)
-		{
-			mapLocation -= 1;
-			localStorage.setItem("playerPosition",mapLocation);
-			console.log(mapLocation);
-		}
-		else
-		{
-			gameMessage = blockedPath[mapLocation];
-		}
-		break;
-		
-		//take command
-		case "take":
-		takeItem();
-		break;
-		
-		//drop command
-		case "drop":
-		dropItem();
-		break;
-		
-		//use command
-		case "use":
-		useItem();
-		break;
-		
-		//help command
-		case "help":
-        if(helpMessages[mapLocation] !== "")
-          {
-            gameMessage = helpMessages[mapLocation] + " ";
-			gameMessage += "<b><br><i>Type instructions if you need game instructions. </i></b>";
-          }
-        break;
-		
-		//instructions command
-		case "instructions":
-		gameMessage = "<i>To explore the home, you will want to use words north, south, east and west. <br></i>";
-		gameMessage += "<i>If you come across an item, be sure to type the word take followed by the item name.</i>";
-		break;
-		
-		default:
-		gameMessage ="This is not a valid option.";
-	}
-	
-	//render the game
-	render();
+function playGame() {
+  //get the players input and convert it to lowercase
+  userInput = input.value;
+  userInput = userInput.toLowerCase();
+
+  //reset these variables from the previous turn
+  gameMessage = "";
+  action = "";
+
+  //figure out the players action
+  for (var i = 0; i < actionsIKnow.length; i++) {
+    if (userInput.indexOf(actionsIKnow[i]) !== -1) {
+      action = actionsIKnow[i];
+      console.log("player's action: " + action);
+      break;
+    }
+  }
+
+  //figure out the item the player wants
+  for (i = 0; i < itemsIKnow.length; i++) {
+    if (userInput.indexOf(itemsIKnow[i]) !== -1) {
+      item = itemsIKnow[i];
+      console.log("Player's item: " + item);
+    }
+  }
+
+  //choose the correct action
+  switch (action) {
+    //move north command
+    case "north":
+      if (mapLocation >= 3) {
+        mapLocation -= 3;
+        localStorage.setItem("playerPosition", mapLocation);
+        console.log(mapLocation);
+      } else {
+        gameMessage = blockedPath[mapLocation];
+      }
+      break;
+
+    //move east command
+    case "east":
+      if (mapLocation % 3 != 2) {
+        mapLocation += 1;
+        localStorage.setItem("playerPosition", mapLocation);
+        console.log(mapLocation);
+      } else {
+        gameMessage = blockedPath[mapLocation];
+      }
+      break;
+
+    //move south command
+    case "south":
+      if (mapLocation < 6) {
+        mapLocation += 3;
+        localStorage.setItem("playerPosition", mapLocation);
+        console.log(mapLocation);
+      } else {
+        gameMessage = blockedPath[mapLocation];
+      }
+      break;
+
+    //move west command
+    case "west":
+      if (mapLocation % 3 != 0) {
+        mapLocation -= 1;
+        localStorage.setItem("playerPosition", mapLocation);
+        console.log(mapLocation);
+      } else {
+        gameMessage = blockedPath[mapLocation];
+      }
+      break;
+
+    //take command
+    case "take":
+      takeItem();
+      break;
+
+    //drop command
+    case "drop":
+      dropItem();
+      break;
+
+    //use command
+    case "use":
+      useItem();
+      break;
+
+    //help command
+    case "help":
+      if (helpMessages[mapLocation] !== "") {
+        gameMessage = helpMessages[mapLocation] + " ";
+        gameMessage += "<b><br><i>Type instructions if you need game instructions. </i></b>";
+      }
+      break;
+
+    //instructions command
+    case "instructions":
+      gameMessage = "<i>To explore the home, you will want to use words north, south, east and west. <br></i>";
+      gameMessage += "<i>If you come across an item, be sure to type the word take followed by the item name.</i>";
+      break;
+
+    default:
+      gameMessage = "This is not a valid option.";
+  }
+
+  //render the game
+  render();
 }
 
-function takeItem()
-{
-	//find the index number of the item in the items array
-	var itemIndexNumber = gameItems.indexOf(item);
-	//does the item exist in the game world and is it at the players current location?
-	if(itemIndexNumber !== -1 && itemLocations[itemIndexNumber] === mapLocation)
-	{
-		gameMessage = "You take the " + item + " and place it into your backpack.";
-		//add the item to the players backpack
-		backpack.push(item);
-		
-		//remove the item from the game world
-		gameItems.splice(itemIndexNumber, 1);
-		itemLocations.splice(itemIndexNumber, 1);
-		console.log(itemIndexNumber);
-		console.log(item);
-		
-		//display in the console for testing
-		console.log("World items: " + gameItems);
-		console.log("backpack items: " + backpack);	
-	}
-	else
-	{
-		//message if the player tries to take an item that isn't in the current location
-		gameMessage = "you can't do that.";
-	}
+function takeItem() {
+  //find the index number of the item in the items array
+  var itemIndexNumber = gameItems.indexOf(item);
+  //does the item exist in the game world and is it at the players current location?
+  if (itemIndexNumber !== -1 && itemLocations[itemIndexNumber] === mapLocation) {
+    gameMessage = "You take the " + item + " and place it into your backpack.";
+    //add the item to the players backpack
+    backpack.push(item);
+
+    //remove the item from the game world
+    gameItems.splice(itemIndexNumber, 1);
+    itemLocations.splice(itemIndexNumber, 1);
+    console.log(itemIndexNumber);
+    console.log(item);
+
+    //display in the console for testing
+    console.log("World items: " + gameItems);
+    console.log("backpack items: " + backpack);
+  } else {
+    //message if the player tries to take an item that isn't in the current location
+    gameMessage = "you can't do that.";
+  }
 }
 
 //drop item function
-function dropItem()
-{
-	//try to drop the item only if the backpack isn't empty
-	if(backpack.length !== 0)
-	{
-		//find the item's array index number in the backpack
-		var backpackIndexNumber = backpack.indexOf(item);
-		//the item is in the backpack if the backpackIndexNumber isn't -1
-		if(backpackIndexNumber !== -1)
-		{
-			//tell the player that the item has been dropped
-			gameMessage = "You dropped the " + item + ".";
-			
-			//add the item from the backpack to the game world
-			gameItems.push(backpack[backpackIndexNumber]);
-			itemLocations.push(mapLocation);
-			//remove the item from the player's backpack
-			backpack.splice(backpackIndexNumber, 1);
-			
-		}
-		else
-		{
-			//message if the player tries to drop something that is not in the backpack
-			gameMessage = "You can't do that.";
-		}
-	}
-	else
-	{
-		//message if the backpack is empty
-		gameMessage = "You're not carrying anything.";
-	}
+function dropItem() {
+  //try to drop the item only if the backpack isn't empty
+  if (backpack.length !== 0) {
+    //find the item's array index number in the backpack
+    var backpackIndexNumber = backpack.indexOf(item);
+    //the item is in the backpack if the backpackIndexNumber isn't -1
+    if (backpackIndexNumber !== -1) {
+      //tell the player that the item has been dropped
+      gameMessage = "You dropped the " + item + ".";
+
+      //add the item from the backpack to the game world
+      gameItems.push(backpack[backpackIndexNumber]);
+      itemLocations.push(mapLocation);
+      //remove the item from the player's backpack
+      backpack.splice(backpackIndexNumber, 1);
+    } else {
+      //message if the player tries to drop something that is not in the backpack
+      gameMessage = "You can't do that.";
+    }
+  } else {
+    //message if the backpack is empty
+    gameMessage = "You're not carrying anything.";
+  }
 }
 
 //use item function
-function useItem()
-{
-	//find out if the item is in the backpack
-	//find the item's array index number in the backpack
-	var backpackIndexNumber = backpack.indexOf(item);
-	
-	//error message that lets the player know the backpack is empty (-1 means inventory is empty)
-	if(backpackIndexNumber === -1)
-	{
-		gameMessage = "You're not carrying any items.";
-	}
-	//if there are no items in the backpack, then tell the player the backpack is empty
-	if(backpack.length === 0)
-	{
-		gameMessage += "Your backpack is empty";
-	}
-	
-	//if the item is found in the backpack figure out what to do with the item
-	if(backpackIndexNumber !== -1)
-	{
-		switch(item)
-		{
-			//use key
-			case "key":
-			if(mapLocation === 2)
-			{
-				gameMessage = "You use the rusty key to open the chest in the tool shed.";
-				backpack.splice(backpackIndexNumber, 1);
-				//check location of player
-				console.log(mapLocation + " current map location");
-				
-			gameItems.push("candle");
-	        itemLocations.push(mapLocation);
-			console.log(backpackIndexNumber);
-			}
-			else
-			{
-			gameMessage = "You don't see anything in the room that would use the key.";
-			}
-			break;
-			
-			//use cheese
-			case "cheese":
-			if(mapLocation === 6)
-			{
-				gameMessage = "You give the mouse the piece of cheese.  The mouse brings you a picture.  You place the picture in your backpack.";
-				backpack.splice(backpackIndexNumber, 1);
-				//check location of player
-				console.log(mapLocation + " current map location");
-				
-			gameItems.push("picture");
-	        itemLocations.push(mapLocation);
-			console.log(backpackIndexNumber);
-			}
-			else
-			{
-				gameMessage = "Your stomach growls, you eyeball the cheese, but decide not to eat it as it may be useful in a different location of the house.";
-			}
-			break;
-			
-			//use candle
-			case "candle":
-			if(mapLocation === 0)
-			{
-				gameMessage = "You light the candle and place it on the patio table.  The candle emits a lovely glow.";
-				backpack.splice(backpackIndexNumber, 1);
-			}
-			//gameMessage = "You gently place the ancient pocket watch into your bag";
-			break;
-		
-			//use picture
-			case "picture":
-				if(mapLocation === 5)
-				{
-					gameMessage = "You place the picture on the bed side table";
-					//remove item from players backpack
-					backpack.splice(backpackIndexNumber, 1);
-				}
-			else
-			{
-		gameMessage = "You find an old picture of someone's great grandparent.  It is usless to you, but you decide to keep it anyway.";
-			}
-		break;
-		}
-	}
+function useItem() {
+  //find out if the item is in the backpack
+  //find the item's array index number in the backpack
+  var backpackIndexNumber = backpack.indexOf(item);
+
+  //error message that lets the player know the backpack is empty (-1 means inventory is empty)
+  if (backpackIndexNumber === -1) {
+    gameMessage = "You're not carrying any items.";
+  }
+  //if there are no items in the backpack, then tell the player the backpack is empty
+  if (backpack.length === 0) {
+    gameMessage += "Your backpack is empty";
+  }
+
+  //if the item is found in the backpack figure out what to do with the item
+  if (backpackIndexNumber !== -1) {
+    switch (item) {
+      //use key
+      case "key":
+        if (mapLocation === 2) {
+          gameMessage = "You use the rusty key to open the chest in the tool shed.";
+          backpack.splice(backpackIndexNumber, 1);
+          //check location of player
+          console.log(mapLocation + " current map location");
+
+          gameItems.push("candle");
+          itemLocations.push(mapLocation);
+          console.log(backpackIndexNumber);
+        } else {
+          gameMessage = "You don't see anything in the room that would use the key.";
+        }
+        break;
+
+      //use cheese
+      case "cheese":
+        if (mapLocation === 6) {
+          gameMessage = "You give the mouse the piece of cheese.  The mouse brings you a picture.  You place the picture in your backpack.";
+          backpack.splice(backpackIndexNumber, 1);
+          //check location of player
+          console.log(mapLocation + " current map location");
+
+          gameItems.push("picture");
+          itemLocations.push(mapLocation);
+          console.log(backpackIndexNumber);
+        } else {
+          gameMessage = "Your stomach growls, you eyeball the cheese, but decide not to eat it as it may be useful in a different location of the house.";
+        }
+        break;
+
+      //use candle
+      case "candle":
+        if (mapLocation === 0) {
+          gameMessage = "You light the candle and place it on the patio table.  The candle emits a lovely glow.";
+          backpack.splice(backpackIndexNumber, 1);
+        }
+        //gameMessage = "You gently place the ancient pocket watch into your bag";
+        break;
+
+      //use picture
+      case "picture":
+        if (mapLocation === 5) {
+          gameMessage = "You place the picture on the bed side table";
+          //remove item from players backpack
+          backpack.splice(backpackIndexNumber, 1);
+        } else {
+          gameMessage = "You find an old picture of someone's great grandparent.  It is usless to you, but you decide to keep it anyway.";
+        }
+        break;
+    }
+  }
 }
 
 //function that renders the game
-function render()
-	{
-		//This renders the location
-		output.innerHTML = map[mapLocation];
-		image.src = "../images/" + images[mapLocation];
-		
-		//This will display an item if it happens to be in the players location
-		for(var i = 0; i < gameItems.length; i++)
-		{
-			if(mapLocation === itemLocations[i])
-			{
-				output.innerHTML += "<br>You notice a <strong>" + gameItems[i] + "</strong>.  It appears to be useful somehow. <br>";
-			}
-		}
-			
-		//This displays the game message
-		output.innerHTML += "<br>" + gameMessage + "<br>";
-		
-		//This displays the contents in the backpack
-		if(backpack.length !== 0)
-		{
-			output.innerHTML += "<br>Your current backpack inventory: " + backpack.join(", ");
-		}
-		input.value = "";
-	}
+function render() {
+  //This renders the location
+  output.innerHTML = map[mapLocation];
+  image.src = "../images/" + images[mapLocation];
+
+  //This will display an item if it happens to be in the players location
+  for (var i = 0; i < gameItems.length; i++) {
+    if (mapLocation === itemLocations[i]) {
+      output.innerHTML += "<br>You notice a <strong>" + gameItems[i] + "</strong>.  It appears to be useful somehow. <br>";
+    }
+  }
+
+  //This displays the game message
+  output.innerHTML += "<br>" + gameMessage + "<br>";
+
+  //This displays the contents in the backpack
+  if (backpack.length !== 0) {
+    output.innerHTML += "<br>Your current backpack inventory: " + backpack.join(", ");
+  }
+  input.value = "";
+}
