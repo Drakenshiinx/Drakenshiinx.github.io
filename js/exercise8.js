a3480f94d98982913d182a05e5c54cd0c1067dcd
Tiffany Cole <Drakenshiinx@hotmail.com>
2019-03-05 21:48:04 -0800

    exercise 8

---
+++ js/exercise8.js
@@ -0,0 +1,37 @@
+//button 1
+$(function() {
+    "use strict";
+
+    $('#button1').click(function(e) {
+    	e.preventDefault();
+    
+	$('#test')
+	.find('.flowers')
+	.hide(3000);
+    });
+});
+
+//button 2
+$(function() {
+    "use strict";
+
+    $('#button2').click(function(e) {
+    	e.preventDefault();
+    
+	$('#test')
+	.find('.flowers')
+	.show(3000);
+    });
+});
+
+//button 3
+$(function() {
+    "use strict";
+	var $item = $('#test');
+    
+    $('#button3').click(function(e) {
+    	e.preventDefault();
+		
+		$item.find('.flowers').toggle(3000);
+    });
+});
